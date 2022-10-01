30a0ee37e242ff26a121618b9e95b6e9abbcfc5a
Phoenix <phoenix@Phoenixs-MacBook-Air.local>
2022-10-01 23:02:45 +0530

    update on logic to pay seller when product is received

--- purchase.sol
+++ purchase.sol
@@ -32,41 +32,40 @@ contract OnlinePurchaseAgreement {
 
     constructor(uint256 _value) payable {
         require(_value == msg.value, "Please send correct amount. Same as purchase amount");
-       
-        value = _value;
-        
+        value = _value; 
         seller = payable(msg.sender);
     }
 
     function confirmPurchase() external payable inState(PurchaseStatus.CREATED){
-        
         require(msg.value == 2*value, "Please send twice the purchase amount");
-
         buyer = payable(msg.sender);
         purchaseStatus = PurchaseStatus.LOCKED;
-        
-        emit ContractLocked(buyer, seller, value, purchaseStatus);
-        
+        emit ContractLocked(buyer, seller, value, purchaseStatus);   
     }
 
     function confirmReceived() external inState(PurchaseStatus.LOCKED) {
         require(msg.sender==buyer,"Only buyer can call this method");
         purchaseStatus = PurchaseStatus.ITEM_RECEIVED;
-        buyer.transfer(value);
+        sendEther(buyer, value);
+        paySeller();
         emit ItemReceived(buyer, seller, value, purchaseStatus);
     }
 
-    function paySeller() external inState(PurchaseStatus.ITEM_RECEIVED){
-        require(msg.sender==seller,"Only seller can call this method");
+    function paySeller() internal inState(PurchaseStatus.ITEM_RECEIVED) {
         purchaseStatus = PurchaseStatus.COMPLETED;
-        seller.transfer(2*value);
+        sendEther(seller, value);
         emit ContractCompleted(buyer, seller, value, purchaseStatus);
     }
 
     function abort() external inState(PurchaseStatus.CREATED){
-        require(msg.sender==seller,"Onlyseller can call this method");
+        require(msg.sender==seller,"Only seller can call this method");
         purchaseStatus = PurchaseStatus.INACTIVE;
-        seller.transfer(value);
+        sendEther(seller, value);
         emit ContractAborted(buyer, purchaseStatus);
+    }
+
+    function sendEther(address payable to, uint256 _value) internal {
+        bool sent = to.send(_value);
+        require(sent, "Failed to transfer eth");
     }
 }
